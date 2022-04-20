4a7bfe3b387e8595fc2d86cc0d21c907d371bb12
Saurabh Pandey <83428329+saurabhpandey5136@users.noreply.github.com>
2022-04-21 00:39:52 +0530

    Update login.js

--- login.js
+++ login.js
@@ -4,14 +4,11 @@ function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "scott" && password == "tiger"){
-alert ("Login successfully");
 window.location = "otherpages/main.html"; // Redirecting to other page.
 return false;
 }
 else{
 attempt --;// Decrementing by one.
-alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
 if( attempt == 0){
 document.getElementById("username").disabled = true;
 document.getElementById("password").disabled = true;
